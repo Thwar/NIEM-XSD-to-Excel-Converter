f48a6605b14bc3288dbc027ca6b70cbf67f5157f
Thwar/NIEM-XSD-to-Excel-Converter <thomaswar3@gmail.com>
2015-07-10 13:42:53 -0400

    new

---
+++ NIEMXML/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace NIEMXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.selectXSD = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createExcel = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(26, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // selectXSD
+            // 
+            this.selectXSD.Location = new System.Drawing.Point(26, 63);
+            this.selectXSD.Name = "selectXSD";
+            this.selectXSD.Size = new System.Drawing.Size(230, 26);
+            this.selectXSD.TabIndex = 2;
+            this.selectXSD.Text = "Select NIEM XSD";
+            this.selectXSD.UseVisualStyleBackColor = true;
+            this.selectXSD.Click += new System.EventHandler(this.selectXSD_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Menu;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.BackColor = System.Drawing.SystemColors.Menu;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // createExcel
+            // 
+            this.createExcel.Enabled = false;
+            this.createExcel.Location = new System.Drawing.Point(26, 117);
+            this.createExcel.Name = "createExcel";
+            this.createExcel.Size = new System.Drawing.Size(169, 40);
+            this.createExcel.TabIndex = 4;
+            this.createExcel.Text = "Convert To Excel";
+            this.createExcel.UseVisualStyleBackColor = true;
+            this.createExcel.Click += new System.EventHandler(this.createExcel_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 168);
+            this.progressBar1.MarqueeAnimationSpeed = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(230, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 201);
+            this.label1.MaximumSize = new System.Drawing.Size(100, 0);
+            this.label1.MinimumSize = new System.Drawing.Size(250, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Step 1: Select XSD to convert";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(54, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(16, 218);
+            this.label2.MinimumSize = new System.Drawing.Size(250, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 13);
+            this.label2.TabIndex = 8;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 251);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.createExcel);
+            this.Controls.Add(this.selectXSD);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NIEM XSD to Excel Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button selectXSD;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button createExcel;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ NIEMXML/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace NIEMXML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+         
+            InitializeComponent();
+        }
+
+        string elementName = "";
+        string extensionClass = "";
+        XNamespace xs = XNamespace.Get("http://www.w3.org/2001/XMLSchema");
+        XDocument doc;
+        bool jobCanceled = false;
+        string errorMsg;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Get Element Types
+        public static string searchForElementTypes(string elementName, XDocument doc, XNamespace xs)
+        {
+            foreach (var el in doc.Root.Elements(xs + "element"))
+            {
+                if (el.Attribute("name").Value == elementName)
+                {
+                    return el.Attribute("type") != null ? el.Attribute("type").Value : "";
+                }
+            }
+            return "";
+        }
+
+
+        //Get Element Documentation
+        public static string searchForElementDocumentation(string elementName, XDocument doc, XNamespace xs)
+        {
+            foreach (var el in doc.Root.Elements(xs + "element"))
+            {
+                if (el.Attribute("name").Value == elementName)
+                {
+                    //Check if Documentation exists for Class
+                    if (el.Elements(xs + "annotation").Elements(xs + "documentation").Count() != 0)
+                        return el.Elements(xs + "annotation").Elements(xs + "documentation").Single().Value;
+                }
+            }
+            return "";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectXSD_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "XSD NIEM (.xsd)|*.xsd";
+            openFileDialog1.FilterIndex = 1;
+
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                textBox1.Text = file;
+
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    createExcel.Enabled = true;
+                    label1.Text = "Step 2: Click Convert To Excel button to begin."; 
+                
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This tool is used to convert NIEM XSD schemas into Excel Spreadsheets. \nAuthor: Ruben T. Rosales\n\nVersion v1.0", "About NIEM XSD to Excel Converter ", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void createExcel_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            progressBar1.MarqueeAnimationSpeed = 30;
+            createExcel.Enabled = false;
+            selectXSD.Enabled = false;
+            label1.Text = "Creating Excel Spreadsheet. Please wait...";
+            label2.Text = "Do not close or click on Excel. \nDoing so will terminate the operation.";
+
+            backgroundWorker1.RunWorkerAsync();
+
+
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+
+            jobCanceled = false;
+            errorMsg = "";
+            CreateExcelDoc excell_app = new CreateExcelDoc();
+            doc = XDocument.Load(openFileDialog1.FileName);
+
+            //creates the main header
+            excell_app.createHeaders(2, 1, openFileDialog1.FileName, "A2", "D2", 2, "YELLOW", true, 10, "n");
+            //creates subheaders
+            excell_app.createHeaders(1, 1, "Class Name (Extension Class)", "A1", "A1", 0, "GRAY", true, 10, "");
+            excell_app.createHeaders(1, 2, "Element Name", "B1", "B1", 0, "GRAY", true, 10, "");
+            excell_app.createHeaders(1, 3, "Element Type", "C1", "C1", 0, "GRAY", true, 10, "");
+            excell_app.createHeaders(1, 4, "Documentation ", "D1", "D1", 0, "GRAY", true, 10, "");
+
+            //Row Start
+            int row = 3;
+            string documentation = "";
+
+
+                foreach (var el in doc.Descendants(xs + "complexType"))
+                {
+                    if ((backgroundWorker1.CancellationPending == true))
+                    {
+                        e.Cancel = true;
+                        break;
+                    }
+                  
+                    //Write Class Name
+                    if ((el.Elements(xs + "complexContent")).Count() != 0)
+                     extensionClass = el.Elements(xs + "complexContent").Elements(xs + "extension").Single().Attribute("base").Value;
+
+                    excell_app.addData(row, 1, el.Attribute("name").Value + " (" + extensionClass + ")", "A" + row, "A" + row, "");
+
+                    //Check if Documentation exists for Class
+                    if (el.Elements(xs + "annotation").Elements(xs + "documentation").Count() != 0)
+                    {
+                        documentation = el.Elements(xs + "annotation").Elements(xs + "documentation").Single().Value;
+                        //Write Documentation for Class
+                        excell_app.addData(row, 4, documentation, "D" + row, "D" + row, "");
+                    }
+
+                    row++;
+
+                    foreach (var attr in el.Elements(xs + "complexContent").Elements(xs + "extension").Elements(xs + "sequence").Elements(xs + "element"))
+                    {
+                        if ((backgroundWorker1.CancellationPending == true))
+                        {
+                            e.Cancel = true;
+                            break;
+                        }
+
+                        //Element Name
+                        elementName = attr.Attribute("ref") != null ? attr.Attribute("ref").Value : "";
+                        excell_app.addData(row, 2, elementName, "B" + row, "B" + row, "");
+
+                        //Element Type
+                        elementName = elementName.Substring(elementName.IndexOf(":") + 1);                      
+                        string elementType = searchForElementTypes(elementName, doc, xs);
+                        excell_app.addData(row, 3, elementType, "C" + row, "C" + row, "");
+
+                        //Element Documentation
+                        documentation = searchForElementDocumentation(elementName, doc, xs);
+                        excell_app.addData(row, 4, documentation, "D" + row, "D" + row, "");
+
+                                       
+                        
+                        row++;
+                    }
+                    excell_app.createHeaders(row, 2, "", "A" + row, "D" + row, 2, "GAINSBORO", true, 10, "");
+                    row++;
+                }
+            }
+            catch (System.Runtime.InteropServices.COMException ex)
+            {
+                errorMsg = "You probably clicked on Excel or Closed it. Therefore the operation will now terminate.\nPlease restart the conversion.";
+            }
+            catch(Exception ex) 
+            {
+               errorMsg = ex.ToString();
+            }
+          
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+        
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            createExcel.Enabled = true;
+            selectXSD.Enabled = true;
+            label2.Text = "";
+            label1.Text = "";
+
+            if (errorMsg != "")
+            {
+                MessageBox.Show(errorMsg, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label2.Text = "An Error Ocurred. Please try again.";
+            }
+            else
+            {
+                if (!jobCanceled)
+                {
+                    MessageBox.Show("Done!", "Operation Succesfull", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    label1.Text = "Done!";
+                }
+                else
+                {
+                    MessageBox.Show("Operation was Canceled!", "Operation Canceled", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    label1.Text = "Canceled!";
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            jobCanceled = true;
+            backgroundWorker1.CancelAsync();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
